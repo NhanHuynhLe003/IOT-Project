b2a369d316d57c6717e48eaf960ebb741b4d30d7
NhanHuynh <nhan123huynh2k3@gmail.com>
2022-10-02 10:37:30 +0700

    ver01

---
+++ main.js
@@ -0,0 +1,59 @@
+let weather = {
+    "apiKey": "a4f316c60c467a1ddc52dbef9570f4dd",
+    weatherFetching: function(city) {
+        fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
+        )
+        .then((rep) => rep.json()).then((data) => this.weatherRendering(data));
+    },
+    weatherRendering: function(data) {
+        const {name} = data;
+        const {main, description} = data.weather[0];
+        const {humidity, temp} = data.main;
+        const {speed} = data.wind;
+
+        const date = new Date();
+        const day = date.getDate();
+        const month = date.getMonth()+1;
+        const year = date.getFullYear();
+        const hour = date.getHours();
+        const min = date.getMinutes();
+        const second = date.getSeconds();
+        
+        document.querySelector('.location').innerText = name;
+        document.querySelector('.status-name').innerText = description;
+        document.querySelector('.dgOfHumidity').innerText = humidity + "%";
+        document.querySelector('.dgOfTempa').innerText = temp + "℃";
+        document.querySelector('.dgOfWind').innerText = speed + "km/h";
+
+        const iconStatus = description;
+        if(iconStatus.search('cloud')>=0){
+            document.querySelector('.img-container img').src = `./icon/weather/cloud.svg`
+        }
+        else if(iconStatus.search('clear')>=0){
+            document.querySelector('.img-container img').src = `./icon/weather/clear.svg`
+        }
+        else if(iconStatus.search('rain')>=0){
+            document.querySelector('.img-container img').src = `./icon/weather/rain.svg`
+        }
+        
+        document.querySelector('.hour').innerText = `${hour}:${min}:${second}`
+        document.querySelector('.date').innerText = `${month}/${day}/${year}`
+    },
+    searching: function(){
+        this.weatherFetching(document.querySelector('.search-bar input').value);
+    },
+    loading: setInterval(() => {
+        weather.weatherFetching(`${document.querySelector('.search-bar input').value}`);
+    }, 1000),
+}
+
+    document.querySelector('.search-btn').addEventListener("click", ()=>{
+        weather.searching();
+    });
+
+    
+
+//auto update data
+
+
